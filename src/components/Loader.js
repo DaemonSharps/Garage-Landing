386d17ae2944731be0c52aa3e5c882b4fba6dd39
Денис Смирнов <smirnov.god@gmail.com>
2021-08-28 03:50:17 +0300

    маленькие правки

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Spinner } from 'react-bootstrap'
 
 export const Loader = () =>(
     <div className="d-flex justify-content-center">
-        <div className="spinner-border" role="status">
+        <Spinner animation="border" role="status">
             <span className="visually-hidden">Loading...</span>
-        </div>
+        </Spinner>
     </div>
 )
--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Button, Col, Form, Row } from "react-bootstrap";
-import garJpeg from "../img/places.webp";
-import { PageSection } from "./PageSection";
-import { recordsContext } from "../context/Records/recordsContext";
+import { Button, Col, Form, Row } from "react-bootstrap"
+import garJpeg from "../img/places.webp"
+import { PageSection } from "./PageSection"
+import { recordsContext } from "../context/Records/recordsContext"
 
 export class RegistrationForm extends React.Component{
 
