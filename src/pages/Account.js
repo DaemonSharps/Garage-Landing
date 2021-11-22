724bf6ed132036d71d1d1f8fda84f0730ed40201
DaemonSharps <smirnov.god@gmail.com>
2021-11-23 02:12:56 +0300

    закончил дизайн личного кабинета
    -карточка для добавления записи
    - кнопки для управления записями
    - кнопка редактирования профиля

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,13 +1,17 @@
-import React, { Fragment, useState } from "react"
+import React, { useState } from "react"
 import { Button, Card, Col, Container, Row } from "react-bootstrap"
+import { Loader } from "../components/Loader";
 import { PageSection } from "../components/PageSection"
-import { Record } from "../components/Record"
 
 export const Account = () =>{
     const [records, setRecords] = useState(Array(20).fill(0));
 
-    const onRecordScroll = () =>{
-            setRecords([...records, ...Array(100).fill(1)]);
+    const addRecords = (e) =>{
+        const scroll = e.target.scrollHeight - e.target.scrollTop;
+        const client = e.target.clientHeight;
+        if(scroll - client < 10){
+            setTimeout(() => setRecords([...records, ...Array(10).fill(1)]), 1000);
+        }
     };
     return(
         <Container className="py-5">
@@ -16,34 +20,50 @@ export const Account = () =>{
             <h2 className="headerText"><div>кабинетъ</div></h2>
                 <Row className="py-3">
                     <Col md="4">
-                        <Card className="py-3">
-                            <Card.Body className="text-start">
-                                <p className="g-text-small">Фамилия: Смирнов</p>
-                                <p className="g-text-small">Имя: Денис</p>
-                                <p className="g-text-small">Отчество: Алексеевич</p>
+                        <Card className="py-1 my-1">
+                            <Card.Body className="text-end">
+                                <p className="g-text-small">Смирнов Денис</p>
+                                <p className="g-text-small">Алексеевич</p>
                                 <p className="g-text-small">Почта: smirnov.god@gmail.com</p>
                             </Card.Body>
+                                <Button variant="none" className="m-3 g-btn-brown-dark g-btn-text">Изменить</Button>
                         </Card>
                     </Col>
                     <Col md="8">
-                            <Card className="p-3">
+                            <Card className="p-3 my-1">
                                 <Card.Title>Мои записи</Card.Title>
                             </Card>
-                            <Row scroll onScroll={onRecordScroll}>
+                            <Row onScroll={addRecords} style={{overflow:'scroll', height:'750px'}}>
+                                        <Col md="4">
+                                            <Card className="my-3">
+                                                <Card.Body className="g-text-small text-center">
+                                                <Button variant="none" className="m-2 g-btn-brown-light g-btn-text">&#10010;</Button>
+                                                </Card.Body>
+                                            </Card>
+                                        </Col>
                                         {records
                                         .map(_ => (
                                             <Col md="4">
-                                            <Card className="my-2">
+                                            <Card className="my-3">
                                                 <Card.Body className="g-text-small">
                                                     <p>Место: 1  Время: 22:00</p>
+                                                    <Button variant="none" className="m-2 g-btn-brown-light g-btn-text">Изменить</Button>
+                                                    <Button variant="none" className="m-2 g-btn-brown-dark g-btn-text">Оменить</Button>
                                                 </Card.Body>
-                                                <Card.Footer>
-                                                    <Button>Удалить</Button>
-                                                </Card.Footer>
                                             </Card>
                                             </Col>
                                           ))}
-                                        <Button onClick={onRecordScroll}>Еще записи</Button>
+                                          {records
+                                        .map(_ => (
+                                            <Col md="4">
+                                            <Card className="my-3">
+                                                <Card.Body className="g-text-small">
+                                                    <p>Место: 1  Время: 22:00</p>
+                                                </Card.Body>
+                                            </Card>
+                                            </Col>
+                                          ))}
+                                        <Loader/>
                             </Row>
                     </Col>
                 </Row>
