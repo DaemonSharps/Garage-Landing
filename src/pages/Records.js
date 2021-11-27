850ca8197d17e69a75e1b8b8ce239299d7f7db54
DaemonSharps <smirnov.god@gmail.com>
2021-11-27 03:21:17 +0300

    Вынес логику RecordList в Record компонент

--- src/pages/Records.js
+++ src/pages/Records.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import { RecordList } from '../components/RecordList'
+import React, { useState } from 'react'
+import { Col, Row, Container } from 'react-bootstrap'
+import { PageSection } from '../components/PageSection'
+import { Record } from '../components/Record'
+import { Loader } from '../components/Loader'
+import { RecordFilter } from '../components/Forms/RecordFilter'
 
 export const Records = () =>{
-    return(
-        <Container className="py-5">
-            <RecordList/>
-        </Container>
-    )
+    const [records, setRecords] = useState([]);
+    const [loading, setLoading] = useState(false);
+        return(
+            <Container>
+            <PageSection>
+                <Col md="12">
+                <h2 className="headerText"><div>Поиск записей</div></h2>
+                <RecordFilter setLoading = {setLoading} setRecords={setRecords}/>
+                <Row>
+                    {loading
+                    ?<Loader/>
+                    :records.filter(record => record.recordStateId === 1).map(rec => (
+                      <Record record = {rec}/>
+                    ))}
+                    {records.length === 0 && !loading
+                    ?<h3>Записей по данному фильтру не обнаружено. Попробуйте другие параметры!</h3>
+                    :''}
+                </Row>
+                </Col>
+            </PageSection>
+            </Container>
+        )
 }
+
